5f75170801dcd1d4b46f10d682574b57c06c5023
Jason Watson <jwatson@newerahd.com>
2015-03-28 18:05:03 -0500

    add primary functionality

--- booklist.js
+++ booklist.js
@@ -1,23 +1,56 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+// simple-todos.js
+Books = new Mongo.Collection("books");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("books");
+
+  Template.body.helpers({
+    books: function () {
+      return Books.find({}, {sort: {title: 1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-book": function (event) {
+      // This function is called when the new task form is submitted
+
+      var title = event.target.title.value;
+      var author = event.target.author.value;
+      var description = event.target.description.value;
+      var isbn = event.target.isbn.value;
+
+
+      Books.insert({
+        title: title,
+        author: author,
+        description: description,
+        isbn: isbn,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    },
+
+    "click .delete": function() {
+      Books.remove(this._id);
     }
+
   });
 }
 
 if (Meteor.isServer) {
+  Meteor.publish("books", function () {
+    return Books.find();
+  });
+
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Books.find().count() === 0) {
+      Books.insert({title: "Moby Dick", author: "Herman Melville", description: "What a dick!"});
+    }
   });
 }
